e692ae12951d7d80ff8a257c5d623cd5354bbfc8
raghavsharma677 <81424060+raghavsharma677@users.noreply.github.com>
2021-11-06 18:56:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+
+x = 0;
+y = 0;
+draw_circle = "";
+draw_rect = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start() 
+{
+    document.getElementById("status").innerHTML = "System in litening please speak";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+
+    console.log(event);
+
+ var content = event.results[0][0].transcript;
+ 
+   document.getElementById("status").innerHTML = "The spach is been recognition as:" +content;
+   if (content == "circle") 
+   {
+       x = Math.floor(Math.random() * 900);
+       y = Math.floor(Math.random() * 600);
+       document.getElementById("status").innerHTML = "Started drawing circle";
+       draw_rect = "set";
+   }
+
+   function setup() {
+       canvas = createCanvas(900, 600); 
+   }
+
+   function draw() {
+       if(draw_circle == "set")
+       {
+           radius = Math.floor(math.random() * 100);
+           circle = (x,y,radius);
+           document.getElementById("status").innerHTML = "Circle is drawn. ";
+           draw_circle = "";
+       }
+
+       if(draw_rect == "set")
+       {
+           rect(x,y,70,50)
+           document.getElementById("status").innerHTML = "Rectangle is drawn. ";
+           draw_rect = "";
+       }
+
+   }
+}
